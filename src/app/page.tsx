0c87e4e1a7c0d63f9eca0acd2e9e7aeaa5344991
gabrielluttjohann <gabrielcristianoluttjohann@gmail.com>
2025-02-12 16:10:44 -0300

    code improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { About, Features, Footer, Hero, Services } from "@/sections";
-import Header from "@/sections/Header";
+import { About, Features, Hero, Services } from "@/sections";
 import Cta from "@/sections/Cta";
 import Faq from "@/sections/Faq";
 import Team from "@/sections/Team";
